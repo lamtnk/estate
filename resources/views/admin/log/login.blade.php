0412d6e32da9988906c824cd94c43b093d867d92
huylee47 <huyw300@gmail.com>
2024-12-16 08:44:10 +0700

    Create : login view

---
+++ resources/views/admin/log/login.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tài Lộc | Đăng nhập </title>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/app.css">
+    <link rel="stylesheet" href="assets/css/pages/auth.css">
+</head>
+
+<body>
+    <div id="auth">
+
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                    </div>
+                    <h1 class="auth-title">Đăng nhập</h1>
+                    {{-- <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p> --}}
+
+                    <form action="{{route('login')}}" method="POST">
+                        @csrf
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-xl" name="email" placeholder="nhập E-mail">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-xl" name="password" placeholder="Nhập mật khẩu">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <div class="form-check form-check-lg d-flex align-items-end">
+                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
+                            {{-- <label class="form-check-label text-gray-600" for="flexCheckDefault">
+                                Keep me logged in
+                            </label> --}}
+                        </div>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Đăng nhập</button>
+                    </form>
+                    <div class="text-center mt-5 text-lg fs-4">
+                        {{-- <p class="text-gray-600">Don't have an account? <a href="auth-register.html"
+                                class="font-bold">Sign
+                                up</a>.</p> --}}
+                        <p><a class="font-bold" href="auth-forgot-password.html">Quên mật khẩu ?</a>.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
