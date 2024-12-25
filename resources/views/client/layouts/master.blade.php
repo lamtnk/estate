afca78c079cd066f10a8c0a2af42c8026122c188
nhat191024 <richberchannel01@gmail.com>
2024-12-25 13:36:58 +0700

    translate text to vn in footer and navbar

--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -45,7 +45,7 @@
     <!-- Body content -->
 
 
-    <div class="header-connect">
+    {{-- <div class="header-connect">
         <div class="container">
             <div class="row">
                 <div class="col-md-5 col-sm-8  col-xs-12">
@@ -70,7 +70,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
     <!--End top header -->
 
     <nav class="navbar navbar-default ">
@@ -89,117 +89,14 @@
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse yamm" id="navigation">
-                <div class="button navbar-right">
-                    <button class="navbar-btn nav-button wow bounceInRight login"
-                        onclick=" window.open('register.html')" data-wow-delay="0.4s">Login</button>
-                    <button class="navbar-btn nav-button wow fadeInRight"
-                        onclick=" window.open('submit-property.html')" data-wow-delay="0.5s">Submit</button>
-                </div>
                 <ul class="main-nav nav navbar-nav navbar-right">
-                    <li class="dropdown ymm-sw " data-wow-delay="0.1s">
-                        <a href="index.html" class="dropdown-toggle active" data-toggle="dropdown"
-                            data-hover="dropdown" data-delay="200">Home <b class="caret"></b></a>
-                        <ul class="dropdown-menu navbar-nav">
-                            <li>
-                                <a href="index-2.html">Home Style 2</a>
-                            </li>
-                            <li>
-                                <a href="index-3.html">Home Style 3</a>
-                            </li>
-                            <li>
-                                <a href="index-4.html">Home Style 4</a>
-                            </li>
-                            <li>
-                                <a href="index-5.html">Home Style 5</a>
-                            </li>
-
-                        </ul>
-                    </li>
-
                     <li class="wow fadeInDown" data-wow-delay="0.1s"><a class=""
-                            href="{{ route('client.property.index') }}">Properties</a></li>
+                            href="{{ route('client.property.index') }}">Trang chủ</a></li>
                     <li class="wow fadeInDown" data-wow-delay="0.1s"><a class=""
-                            href="property.html">Property</a></li>
-                    <li class="dropdown yamm-fw" data-wow-delay="0.1s">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown"
-                            data-delay="200">Template <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <div class="yamm-content">
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <h5>Home pages</h5>
-                                            <ul>
-                                                <li>
-                                                    <a href="index.html">Home Style 1</a>
-                                                </li>
-                                                <li>
-                                                    <a href="index-2.html">Home Style 2</a>
-                                                </li>
-                                                <li>
-                                                    <a href="index-3.html">Home Style 3</a>
-                                                </li>
-                                                <li>
-                                                    <a href="index-4.html">Home Style 4</a>
-                                                </li>
-                                                <li>
-                                                    <a href="index-5.html">Home Style 5</a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-3">
-                                            <h5>Pages and blog</h5>
-                                            <ul>
-                                                <li><a href="blog.html">Blog listing</a> </li>
-                                                <li><a href="single.html">Blog Post (full)</a> </li>
-                                                <li><a href="single-right.html">Blog Post (Right)</a> </li>
-                                                <li><a href="single-left.html">Blog Post (left)</a> </li>
-                                                <li><a href="contact.html">Contact style (1)</a> </li>
-                                                <li><a href="contact-3.html">Contact style (2)</a> </li>
-                                                <li><a href="contact_3.html">Contact style (3)</a> </li>
-                                                <li><a href="faq.html">FAQ page</a> </li>
-                                                <li><a href="404.html">404 page</a> </li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-3">
-                                            <h5>Property</h5>
-                                            <ul>
-                                                <li><a href="property-1.html">Property pages style (1)</a> </li>
-                                                <li><a href="property-2.html">Property pages style (2)</a> </li>
-                                                <li><a href="property-3.html">Property pages style (3)</a> </li>
-                                            </ul>
-
-                                            <h5>Properties list</h5>
-                                            <ul>
-                                                <li><a href="properties.html">Properties list style (1)</a> </li>
-                                                <li><a href="properties-2.html">Properties list style (2)</a> </li>
-                                                <li><a href="properties-3.html">Properties list style (3)</a> </li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-3">
-                                            <h5>Property process</h5>
-                                            <ul>
-                                                <li><a href="submit-property.html">Submit - step 1</a> </li>
-                                                <li><a href="submit-property.html">Submit - step 2</a> </li>
-                                                <li><a href="submit-property.html">Submit - step 3</a> </li>
-                                            </ul>
-                                            <h5>User account</h5>
-                                            <ul>
-                                                <li><a href="register.html">Register / login</a> </li>
-                                                <li><a href="user-properties.html">Your properties</a> </li>
-                                                <li><a href="submit-property.html">Submit property</a> </li>
-                                                <li><a href="change-password.html">Change password</a> </li>
-                                                <li><a href="user-profile.html">Your profile</a> </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- /.yamm-content -->
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li class="wow fadeInDown" data-wow-delay="0.4s"><a href="contact.html">Contact</a></li>
+                            href="{{ route('client.property.index') }}">Bất động sản</a></li>
+                    <li class="wow fadeInDown" data-wow-delay="0.1s"><a class=""
+                            href="{{ route('client.news.index') }}">Tin tức</a></li>
+                    <li class="wow fadeInDown" data-wow-delay="0.4s"><a href="contact.html">Liên hệ</a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
@@ -215,30 +112,30 @@
 
                     <div class="col-md-3 col-sm-6 wow fadeInRight animated">
                         <div class="single-footer">
-                            <h4>About us </h4>
+                            <h4>Về chúng tôi </h4>
                             <div class="footer-title-line"></div>
 
-                            <img src="{{ url('') }}/cassets/img/footer-logo.png" alt=""
-                                class="wow pulse" data-wow-delay="1s">
-                            <p>Lorem ipsum dolor cum necessitatibus su quisquam molestias. Vel unde, blanditiis.</p>
+                            <img src="{{ url('') }}/cassets/img/footer-logo.png" alt="" class="wow pulse"
+                                data-wow-delay="1s">
+                            <p>Tài Lộc Property là một công ty chuyên cung cấp các dịch vụ bất động sản uy tín và
+                                chuyên nghiệp. Với sứ mệnh mang lại giá trị bền vững cho khách hàng, chúng tôi tự hào là
+                                đối tác đáng tin cậy trong việc tư vấn, mua bán, cho thuê và đầu tư bất động sản.</p>
                             <ul class="footer-adress">
                                 <li><i class="pe-7s-map-marker strong"> </i> 9089 your adress her</li>
                                 <li><i class="pe-7s-mail strong"> </i> email@yourcompany.com</li>
-                                <li><i class="pe-7s-call strong"> </i> +1 908 967 5906</li>
+                                <li><i class="pe-7s-call strong"> </i> 0936 622 111</li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-md-3 col-sm-6 wow fadeInRight animated">
                         <div class="single-footer">
-                            <h4>Quick links </h4>
+                            <h4>Truy cập nhanh </h4>
                             <div class="footer-title-line"></div>
                             <ul class="footer-menu">
-                                <li><a href="properties.html">Properties</a> </li>
-                                <li><a href="#">Services</a> </li>
-                                <li><a href="submit-property.html">Submit property </a></li>
-                                <li><a href="contact.html">Contact us</a></li>
-                                <li><a href="faq.html">fqa</a> </li>
-                                <li><a href="faq.html">Terms </a> </li>
+                                <li><a href="#">Bất động sản</a> </li>
+                                <li><a href="#">Dự án</a> </li>
+                                <li><a href="#">Liên hệ</a></li>
+                                <li><a href="#">FAQ</a> </li>
                             </ul>
                         </div>
                     </div>
@@ -298,10 +195,11 @@
                     </div>
                     <div class="col-md-3 col-sm-6 wow fadeInRight animated">
                         <div class="single-footer news-letter">
-                            <h4>Stay in touch</h4>
+                            <h4>Kết nối với chúng tôi</h4>
                             <div class="footer-title-line"></div>
-                            <p>Lorem ipsum dolor sit amet, nulla suscipit similique quisquam molestias. Vel unde,
-                                blanditiis.</p>
+                            <p>Chúng tôi luôn sẵn sàng lắng nghe ý kiến và hỗ trợ bạn. Hãy kết nối với chúng tôi qua các
+                                kênh mạng xã hội, email hoặc hotline để nhận được sự tư vấn và giải đáp nhanh chóng.
+                                Đừng ngần ngại liên hệ, vì sự hài lòng của bạn là ưu tiên hàng đầu của chúng tôi.</p>
 
                             <form>
                                 <div class="input-group">
@@ -316,15 +214,15 @@
 
                             <div class="social pull-right">
                                 <ul>
-                                    <li><a class="wow fadeInUp animated" href="https://twitter.com/kimarotec"><i
+                                    <li><a class="wow fadeInUp animated" href="https://www.facebook.com/p/T%C3%A0i-L%E1%BB%99c-Property-61561046392320/?locale=nn_NO"><i
                                                 class="fa fa-twitter"></i></a></li>
-                                    <li><a class="wow fadeInUp animated" href="https://www.facebook.com/kimarotec"
+                                    <li><a class="wow fadeInUp animated" href="https://www.facebook.com/p/T%C3%A0i-L%E1%BB%99c-Property-61561046392320/?locale=nn_NO"
                                             data-wow-delay="0.2s"><i class="fa fa-facebook"></i></a></li>
-                                    <li><a class="wow fadeInUp animated" href="https://plus.google.com/kimarotec"
+                                    <li><a class="wow fadeInUp animated" href="hhttps://www.facebook.com/p/T%C3%A0i-L%E1%BB%99c-Property-61561046392320/?locale=nn_NO"
                                             data-wow-delay="0.3s"><i class="fa fa-google-plus"></i></a></li>
-                                    <li><a class="wow fadeInUp animated" href="https://instagram.com/kimarotec"
+                                    <li><a class="wow fadeInUp animated" href="https://www.facebook.com/p/T%C3%A0i-L%E1%BB%99c-Property-61561046392320/?locale=nn_NO"
                                             data-wow-delay="0.4s"><i class="fa fa-instagram"></i></a></li>
-                                    <li><a class="wow fadeInUp animated" href="https://instagram.com/kimarotec"
+                                    <li><a class="wow fadeInUp animated" href="https://www.facebook.com/p/T%C3%A0i-L%E1%BB%99c-Property-61561046392320/?locale=nn_NO"
                                             data-wow-delay="0.6s"><i class="fa fa-dribbble"></i></a></li>
                                 </ul>
                             </div>
@@ -339,7 +237,7 @@
             <div class="container">
                 <div class="row">
                     <div class="pull-left">
-                        <span> (C) <a href="http://www.KimaroTec.com">KimaroTheme</a> , All rights reserved 2016
+                        <span> (C) <a href="http://www.KimaroTec.com">FPT Polytechnic</a> , All rights reserved 2024
                         </span>
                     </div>
                     <div class="bottom-menu pull-right">
